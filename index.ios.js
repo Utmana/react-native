68b1db349c224e57d0a3f01a513debb7a372becc
Radu Achim <radu.achim@iteam.se>
2015-04-22 20:37:25 +0200

    Bit of refactor, bit of cleanup, added a side-menu

--- index.ios.js
+++ index.ios.js
@@ -5,43 +5,78 @@
 'use strict';
 
 var React = require('react-native');
+var SideMenu = require('react-native-side-menu');
+var window = require('Dimensions').get('window');
+
 var {
   AppRegistry,
   StyleSheet,
   NavigatorIOS,
-  Route
+  Route,
+  Text,
+  View
 } = React;
-
-var {
-  BASE_URL
-} = require('./lib/config');
-
-var fetch = require('fetch');
 
 var Challenges = require('./lib/components/challenges');
 var About = require('./lib/components/about');
 
-var UtmanaProject = React.createClass({
-  render: function() {
-
+var Menu = React.createClass({
+  transitionTo(route) {
+    this.props.menuActions.close();
+    this.props.navigator.push(route);
+  },
+  render() {
     return (
-      <NavigatorIOS
-        style={styles.container}
-        initialRoute={{
-          title: 'Challenges',
-          component: Challenges
-        }}
-      >
-        <Route
-          title='About'
-          component={{About}}
-        />
-      </NavigatorIOS>
+      <View style={styles.menu}>
+        <Text
+          style={styles.item}
+         >
+          Challenges
+        </Text>
+        <Text
+          style={styles.item}
+          onPress={this.transitionTo.bind({}, {
+            title: 'About',
+            component: About
+          })}>
+        About
+        </Text>
+      </View>
+    );
+  }
+});
+
+var UtmanaProject = React.createClass({
+  render() {
+    return (
+      <SideMenu menu={<Menu />}>
+        <NavigatorIOS
+          style={styles.container}
+          initialRoute={{
+            title: 'Challenges',
+            component: Challenges
+          }}
+        >
+        </NavigatorIOS>
+      </SideMenu>
     );
   }
 });
 
 var styles = StyleSheet.create({
+  menu: {
+    flex: 1,
+    width: window.width,
+    height: window.height,
+    // backgroundColor: 'gray',
+    justifyContent: 'center',
+    padding: 20
+  },
+  item: {
+    fontSize: 18,
+    fontWeight: '300',
+    paddingTop: 5,
+  },
   container: {
     flex: 1,
     backgroundColor: 'white',
